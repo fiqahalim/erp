ace7ab3221ec54a6d4b6cb7c9ba5bbfc8380c4b4
mushroom <syafiqahalim.work@gmail.com>
2022-08-20 11:01:31 +0000

    additional migration file for purchases table

---
+++ packages/Webkul/Core/src/Database/Migrations/2022_08_20_183956_create_transaction_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('transaction_name')->nullable();
+            $table->string('transaction_code')->nullable();
+            $table->decimal('amount', 12,2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_types');
+    }
+}
